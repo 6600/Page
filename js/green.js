d324602b822ff418642695340314e015414dfe94
HPH <HPH@HPH-PC>
2016-12-09 17:30:11 +0800

    撸了个绿版的简历。

---
+++ js/green.js
@@ -0,0 +1,14 @@
+/**
+ * 蓝色背景。
+ */
+$(window).load(function(){
+    $('.preloader').delay(1000).fadeOut("slow");   
+});
+
+$(function(){
+    jQuery(document).ready(function() {
+		$('body').backstretch([
+	 		 "images/tm-bg-slide-2.jpg",
+	 			], 	{duration: 3200, fade: 1300});
+		});
+})
